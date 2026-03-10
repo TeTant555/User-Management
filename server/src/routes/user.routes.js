048b22f9f7c6fcaa79f6793da352a8b58725437a
TeTant555 <thettetantaung@gmail.com>
2026-03-10 09:47:30 +0630

    changed

--- server/src/routes/user.routes.js
+++ server/src/routes/user.routes.js
@@ -3,6 +3,7 @@ const controller = require('../controllers/user.controller');
 
 const router = express.Router();
 
+// Authentication and User Management Routes
 router.get('/users', controller.getUsers);
 router.get('/userById/:id', controller.getUser);
 router.post('/login', controller.login);
